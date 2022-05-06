cbc96847a886e03a16b2ed91e25124f239f4ae6c
alexdmitriew <admitriew272@technikumkreatywne.pl>
2022-05-06 13:51:37 +0200

    ciag fibonaciego

---
+++ ciag fibonaciego/ciag fibonaciego/ciag fibonaciego.cpp
@@ -0,0 +1,43 @@
+﻿// ConsoleApplication1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int n;
+	int f;
+	int f0 = 0;
+	int f1 = 1;
+
+	cout << "ile wyrazow ciagu wypisac: ";
+	cin >> n;
+	for (int i = 0; i <= n; i++) {
+		if (i > 1) {
+			f = f0 + f1;
+			f0 = f1;
+			f1 = f;
+		}
+
+
+		else {
+			f = i;
+
+		}
+
+		cout << f << endl;
+	}
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
