3d29a9837f4ca23f75b5ca8967d140dc60310d32
alexdmitriew <100200576+alexdmitriew@users.noreply.github.com>
2022-05-23 11:28:23 +0200

    Wyszukiwanie binarne

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,47 @@
+﻿// ConsoleApplication3.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int l = 0;
+	int p = 15;
+	int	sr = (l + p)/2;
+	int tab[15] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 };
+	int szukana;
+
+	cout << "Podaj liczbe ktora chcesz znalezc:";
+	cin >> szukana;
+
+
+	while (l <= p)
+	{
+		if (tab[sr] == szukana)
+			{
+				cout << "Liczba: " << szukana << " wystepuje w zbiorze w komorce o indeksie: " << sr;
+
+			}
+		if (tab[sr] > szukana)
+			{
+				p = sr - 1;
+			}
+		else l = sr + 1;
+
+
+		sr = (l + p)/2;
+
+	}
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
